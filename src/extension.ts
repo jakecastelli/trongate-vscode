48aa1c97d079023d1f979ab849a4726951d48bd7
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2020-10-14 14:39:22 +1030

    Persist user's Nitro framework selection (#4)
    * Added persist user's framework option
    
    * Now trongate extension can store user's selection and automatically load
    it into Nitro
    
    * Some code refactor and performance boost
    
    * changed the command name

--- src/extension.ts
+++ src/extension.ts
@@ -3,24 +3,27 @@
 import * as vscode from "vscode";
 import { newModule } from "./commands";
 import { switchSnippet } from "./commands";
+import {cssFramework} from './commands/switch-frontend-snippet.comand';
 
-let userFrameworkOption = "Boostrap 4";
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "trongate" is now active!');
+  // Get the user's nitro framework option
+  let userFrameworkOption = vscode.workspace.getConfiguration().get('trongate.userFrameworkOption');
+  //@ts-ignore
+  if (!Object.keys(cssFramework).includes(userFrameworkOption)) {
+    // If the user messed with the settings, we put them back to default
+    userFrameworkOption = "Bootstrap 4";
+  }
+  vscode.window.setStatusBarMessage(`${userFrameworkOption} in use`);
+  // console.log(vscode.workspace.getConfiguration().get('trongate.userFrameworkOption'))
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let nitro = vscode.commands.registerCommand(
-    "trongate.selectFramework",
+    "trongate.insertSnippet",
     (args) => {
-      console.log(args);
-      console.log(userFrameworkOption);
-      console.log(" --- --- --- ");
       vscode.commands.executeCommand(`editor.action.insertSnippet`, {
         name: `${userFrameworkOption} ${args.name}`,
       });
@@ -34,18 +37,19 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   let selectSnippet = vscode.commands.registerCommand(
-    "trongate.selectSnippet",
+    "trongate.selectNitroFramework",
     // The code you place here will be executed every time your command is executed
-    () => {
+   () => {
       //TODO: Implement the function
       //CURRENTLY:
-      switchSnippet().then((opt) => {
+      switchSnippet().then(async (opt) => {
         if (!opt) {return;}
         //@ts-ignore
         userFrameworkOption = opt;
         vscode.window.showInformationMessage(
           `You have successfully selected ${userFrameworkOption} Framework`
         );
+        await vscode.workspace.getConfiguration().update('trongate.userFrameworkOption', userFrameworkOption, vscode.ConfigurationTarget.Global)
         vscode.window.setStatusBarMessage(`${userFrameworkOption} in use`);
       });
     }
