b98f001c0de294a1407bfa6a6a9e307c99b6f19f
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2020-11-22 12:31:29 +1030

    feat: complete test cases, code refactor, adjust cursor position (#18)
    * - test: complete test cases
    
    Starting to complete test cases, for better maintain and
    code refactor purpose
    
    - Added test cases, refactor the code
    
    * Added few test cases to ensure the controller template format is absolutely
    correct
    
    * Adjusted the cursor initial position if the user chooses to have a view
      template
    
    * Removed redundant method - code refactor
    
    - Small changes
    
    * Adjust the cursor position a bit more
    
    * Added one more test case
    
    * Removed unused variable in switch-front-snippets file - code refactor
    
    * reformatted the test file
    
    * Simplified the view template
    
    * As discussed before, the view template file is a temporary file to let
      the user know the stuff works, and most of the time the user will
      directly remove everything within the view file along with the
      custom.css file, so we do not want the user to go here and
      there just to get started.
    
    * So, the simplified version will only have few lines of code within
      view file which tells the user the stuff works and also links to the
      assets/css/custom.css and assets/js/custom.js
      both custom.css and custom.js are empty.

---
+++ src/test/unit/template.test.ts
@@ -0,0 +1,43 @@
+import { getTongateControllerTemplate } from "../../commands/templates/index";
+
+describe('getTongateControllerTemplate method exists', () => {
+})
+
+describe("make sure the controller template is generated correctly", () => {
+
+    test('getTongateControllerTemplate method exists', () => {
+        expect(typeof getTongateControllerTemplate).toEqual('function')
+    })
+
+    test("module name: store_items, view file: null", () => {
+        const test_moduleName = "store_items";
+        expect(getTongateControllerTemplate(test_moduleName, undefined)).toEqual(
+            `<?php
+class Store_items extends Trongate {
+    
+}`);
+    });
+
+    test("module name: store_items, view file: display", () => {
+        const test_moduleName = "store_items";
+        const test_viewFileName = "display";
+        expect(getTongateControllerTemplate(test_moduleName, test_viewFileName)).toEqual(result);
+    });
+});
+
+
+let result = `<?php
+class Store_items extends Trongate {
+  
+    function index () {
+        $data['view_module'] = 'store_items';
+        $this->view('display', $data);
+        /* Uncomment the lines below, 
+         * Change the template method name, 
+         * Remove lines above, if you want to load to the template
+         */
+        //$data['view_module'] = 'store_items';
+        //$data['view_file'] = 'display';
+        //$this->template('template method here', $data);
+    }  
+}`
