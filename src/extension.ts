cedee5358e124ed0f4e8c6e6c9d785271f48c38c
jakecastelli <959672929@qq.com>
2021-10-17 00:52:59 +1030

    feat: #37 make status bar item clickable - to trigger CSS framework selection dropdown

--- src/extension.ts
+++ src/extension.ts
@@ -2,53 +2,54 @@
 import * as vscode from "vscode";
 import { newModule } from "./commands";
 import { dropDownList } from "./commands";
-import {cssFramework, cssFrameworkQuickPickOptions} from './commands/switch-frontend-snippet.comand';
+import { cssFramework, cssFrameworkQuickPickOptions } from "./commands/switch-frontend-snippet.comand";
 
+let statusBar: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "trongate" is now active!');
 
   // Get the user's nitro framework option
-  let userFrameworkOption = vscode.workspace.getConfiguration().get('trongate.userFrameworkOption');
+  let userFrameworkOption = vscode.workspace.getConfiguration().get("trongate.userFrameworkOption");
   //@ts-ignore
   if (!Object.keys(cssFramework).includes(userFrameworkOption)) {
     // If the user messed with the settings, we put them back to default
     userFrameworkOption = "Bootstrap 4";
   }
-  vscode.window.setStatusBarMessage(`${userFrameworkOption} in use`);
+  statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  statusBar.text = `${userFrameworkOption} in use`;
+  statusBar.command = "trongate.selectNitroFramework";
+  statusBar.show();
 
-  let nitro = vscode.commands.registerCommand(
-    "trongate.insertSnippet",
-    (args) => {
-      vscode.commands.executeCommand(`editor.action.insertSnippet`, {
-        name: `${userFrameworkOption} ${args.name}`,
-      });
-    }
-  );
+  // vscode.window.setStatusBarMessage(`${userFrameworkOption} in use`);
 
-  let newTrongate = vscode.commands.registerCommand(
-    "trongate.newTrongate",
-    newModule
-  );
+  let nitro = vscode.commands.registerCommand("trongate.insertSnippet", (args) => {
+    vscode.commands.executeCommand(`editor.action.insertSnippet`, {
+      name: `${userFrameworkOption} ${args.name}`,
+    });
+  });
 
-  let selectSnippet = vscode.commands.registerCommand(
-    "trongate.selectNitroFramework",
-   () => {
-      dropDownList(cssFramework, cssFrameworkQuickPickOptions).then(async (opt) => {
-        if (!opt) {return;}
-        //@ts-ignore
-        userFrameworkOption = opt;
-        vscode.window.showInformationMessage(
-          `You have successfully selected ${userFrameworkOption} Framework`
-        );
-        await vscode.workspace.getConfiguration().update('trongate.userFrameworkOption', userFrameworkOption, vscode.ConfigurationTarget.Global)
-        vscode.window.setStatusBarMessage(`${userFrameworkOption} in use`);
-      });
-    }
-  );
+  let newTrongate = vscode.commands.registerCommand("trongate.newTrongate", newModule);
 
-  context.subscriptions.push(nitro, newTrongate, selectSnippet);
+  let selectFramework = vscode.commands.registerCommand("trongate.selectNitroFramework", () => {
+    dropDownList(cssFramework, cssFrameworkQuickPickOptions).then(async (opt) => {
+      if (!opt) {
+        return;
+      }
+      //@ts-ignore
+      userFrameworkOption = opt;
+      vscode.window.showInformationMessage(`You have successfully selected ${userFrameworkOption} Framework`);
+      await vscode.workspace
+        .getConfiguration()
+        .update("trongate.userFrameworkOption", userFrameworkOption, vscode.ConfigurationTarget.Global);
+
+      statusBar.text = `${userFrameworkOption} in use`;
+      statusBar.show();
+    });
+  });
+
+  context.subscriptions.push(nitro, newTrongate, selectFramework);
 }
 
 export function deactivate() {}
