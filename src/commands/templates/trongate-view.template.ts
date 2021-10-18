976140b26fa8b307fcca69123990accfedae9bdb
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2021-10-18 23:54:25 +1030

    Bugfix/tg submit and warning css (#39)
    * fix: #34 framework update
    
    * fix: #32 empty CSS warning

--- src/commands/templates/trongate-view.template.ts
+++ src/commands/templates/trongate-view.template.ts
@@ -1,7 +1,7 @@
 export function viewTemplate(moduleName: string, GLOBAL_SETTINGS: any) {
-    const assetsTriggerPhrase = GLOBAL_SETTINGS['config']['MODULE_ASSETS_TRIGGER']
-    
-    return `<h1>Hello from <span><i>${moduleName}</i></span> module</h1>
+  const assetsTriggerPhrase = GLOBAL_SETTINGS["config"]["MODULE_ASSETS_TRIGGER"];
+
+  return `<h1>Hello from <span><i>${moduleName}</i></span> module</h1>
     <p>This view was generated using Trongate Scaffold & Code Snippets!</p>
         <div>
             <a href="https://github.com/jakecastelli/trongate-vscode" target="_blank">Keep updated here
@@ -13,7 +13,11 @@ export function viewTemplate(moduleName: string, GLOBAL_SETTINGS: any) {
         </div>
 <style>
     /* Link to the ${moduleName}/assets/css/custom.css */
-    @import url('<?= BASE_URL ?>${GLOBAL_SETTINGS['superModule']?`${GLOBAL_SETTINGS['parentModuleName']}-${moduleName}${assetsTriggerPhrase}`:`${moduleName}${assetsTriggerPhrase}`}/css/custom.css');
+    @import url('<?= BASE_URL ?>${
+      GLOBAL_SETTINGS["superModule"]
+        ? `${GLOBAL_SETTINGS["parentModuleName"]}-${moduleName}${assetsTriggerPhrase}`
+        : `${moduleName}${assetsTriggerPhrase}`
+    }/css/custom.css');
 
     body {
         background-color: rgb(251, 178, 165);
@@ -21,9 +25,13 @@ export function viewTemplate(moduleName: string, GLOBAL_SETTINGS: any) {
 </style>
 
 <!-- Link to the assets/js/custom.js -->
-<script src="<?= BASE_URL ?>${GLOBAL_SETTINGS['superModule']?`${GLOBAL_SETTINGS['parentModuleName']}-${moduleName}${assetsTriggerPhrase}`:`${moduleName}${assetsTriggerPhrase}`}/js/custom.js"></script>`;
+<script src="<?= BASE_URL ?>${
+    GLOBAL_SETTINGS["superModule"]
+      ? `${GLOBAL_SETTINGS["parentModuleName"]}-${moduleName}${assetsTriggerPhrase}`
+      : `${moduleName}${assetsTriggerPhrase}`
+  }/js/custom.js"></script>`;
 }
 
 export function getTrongateModuleCss() {
-    return ``;
+  return `/* Put your css here */`;
 }
